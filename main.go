37121caffb62c9e5b203e91c64905c16415ba5c4
aminlo <aminlouiosman@gmail.com>
2025-07-09 18:01:26 +0100

    Integrating websocket

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Group(func(r chi.Router) {
+		r.Use(cors.Handler(cors.Options{
+			// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+			AllowedOrigins: []string{"https://*", "http://*"},
+			// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+			ExposedHeaders:   []string{"Link"},
+			AllowCredentials: false,
+			MaxAge:           300, // Maximum value not ignored by any of major browsers
+		}))
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("Hello World!"))
+		})
+		r.Get("/ws", webshandler)
+	})
+	chi.RegisterMethod("JELLO")
+
+	apiRouter := chi.NewRouter()
+
+	// apiRouter.Get("/articles/{date}-{slug}", getArticle)
+
+	// Mounting the new Sub Router on the main router
+	r.Mount("/api", apiRouter)
+	log.Printf("Starting server on port 3030")
+	http.ListenAndServe(":3000", r)
+
+}
