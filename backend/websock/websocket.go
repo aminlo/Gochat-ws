3935953585d63225dce2118b2530ab13ecccf37c
aminlo <aminlouiosman@gmail.com>
2025-07-11 16:45:18 +0100

    Starting to add uniq

--- backend/websock/websocket.go
+++ backend/websock/websocket.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
@@ -15,21 +16,81 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-func webshandler(w http.ResponseWriter, r *http.Request) {
+type Hub struct {
+	clients    map[*websocket.Conn]bool
+	broadcast  chan []byte
+	register   chan *websocket.Conn
+	unregister chan *websocket.Conn
+	mutex      sync.RWMutex
+}
+
+var hub = &Hub{
+	clients:    make(map[*websocket.Conn]bool),
+	broadcast:  make(chan []byte),
+	register:   make(chan *websocket.Conn),
+	unregister: make(chan *websocket.Conn),
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case conn := <-h.register:
+			h.mutex.Lock()
+			h.clients[conn] = true
+			h.mutex.Unlock()
+			log.Printf("Client connected. Total: %d", len(h.clients))
+			log.Println(conn)
+
+		case conn := <-h.unregister:
+			h.mutex.Lock()
+			if _, ok := h.clients[conn]; ok {
+				delete(h.clients, conn)
+				conn.Close()
+			}
+			h.mutex.Unlock()
+			log.Printf("Client disconnected. Total: %d", len(h.clients))
+
+		case message := <-h.broadcast:
+			h.mutex.RLock()
+			for conn := range h.clients {
+				if err := conn.WriteMessage(websocket.TextMessage, message); err != nil {
+					delete(h.clients, conn)
+					conn.Close()
+				}
+			}
+			h.mutex.RUnlock()
+		}
+	}
+}
+
+func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 		return
 	}
+
+	hub.register <- conn
+
+	defer func() {
+		hub.unregister <- conn
+	}()
+
 	for {
-		messageType, p, err := conn.ReadMessage()
+		_, message, err := conn.ReadMessage()
 		if err != nil {
 			log.Println(err)
-			return
+			break
 		}
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
+		// Broadcast to all clients instead of just echoing back
+		hub.broadcast <- message
 	}
+}
+
+func webshandler(w http.ResponseWriter, r *http.Request) {
+	serveWs(hub, w, r)
+}
+
+func init() {
+	go hub.run()
 }
