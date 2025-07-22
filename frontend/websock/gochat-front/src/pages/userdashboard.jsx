99079aa8be138f888a2956318f81e835e553ee09
aminlo <aminlouiosman@gmail.com>
2025-07-22 23:11:20 +0100

    Userdashboard layout

--- frontend/websock/gochat-front/src/pages/userdashboard.jsx
+++ frontend/websock/gochat-front/src/pages/userdashboard.jsx
@@ -1,10 +1,27 @@
 import { useUser } from '../utils/usercontext';
 import { useNavigate } from 'react-router-dom';
+import { Createchat } from '../utils/userchatconfig'
+import { useState } from "react";
 
 const Userdash = () => {
     const { user } = useUser();
     const date = new Date(user.created_at);
     const formatdate = date.toLocaleString()
+    const [hubname, sethubname] = useState("");
+
+
+    const handleCreateChat = async (e) => {
+        e.preventDefault();
+
+        try {
+            await Createchat(hubname);
+            console.log('Hub make successful!');
+        } catch (error) {
+            console.error("Failed to create chat:", error);
+        }
+    };
+
+
 
     return (
         <div>
@@ -13,10 +30,21 @@ const Userdash = () => {
             <div>{user.id}</div>
             <div>{formatdate}</div>
 
-            <hr></hr>
+            <hr />
             <div>manage ur chats here</div>
+            <div>Create hub</div>
+            <form onSubmit={handleCreateChat}>
+                <input
+                    type="text"
+                    placeholder="chat name!"
+                    value={hubname}
+                    onChange={e => sethubname(e.target.value)}
+                    required
+                />
+                <button type="submit">Create Chat</button>
+            </form>
 
-        </div >
+        </div>
     )
 
 }
---
+++ frontend/websock/gochat-front/src/utils/userchatconfig.js
@@ -0,0 +1,12 @@
+const API_URL = 'http://localhost:3000';
+
+export async function Createchat(hubname) {
+    const response = await fetch(`${API_URL}/au/create`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ hubname })
+    });
+}
