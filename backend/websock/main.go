146bcc46cc9239f361ce86c4e6300ca83651e8bf
aminlo <aminlouiosman@gmail.com>
2025-08-10 16:01:32 +0100

    Test

--- backend/websock/main.go
+++ backend/websock/main.go
@@ -43,7 +43,7 @@ func main() {
 	r.Use(cors.Handler(cors.Options{
 		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
 		AllowedOrigins: []string{"http://localhost:3005"},
-		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true }
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
