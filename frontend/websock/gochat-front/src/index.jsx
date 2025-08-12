91853aa43bd4045a848a7a6ffc9a56164ac1e15c
aminlo <aminlouiosman@gmail.com>
2025-08-12 19:09:50 +0100

    configured to firebase housting

--- frontend/websock/gochat-front/src/index.jsx
+++ frontend/websock/gochat-front/src/index.jsx
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <UserProvider>
-      <BrowserRouter basename="/gochat-front-react">
+      <BrowserRouter>
         <Routes>
           <Route path="/">
             <Route index element={<Home />} />
--- frontend/websock/gochat-front/vite.config.mjs
+++ frontend/websock/gochat-front/vite.config.mjs
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: '/gochat-front-react/',
+    base: './',
     plugins: [react(), tailwindcss()],
     server: {
         port: 3005,
